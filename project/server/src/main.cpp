ed16ffa5461123d29ae8e7d55caec72a1b1dd1ea
Ilya Evseev <olaoshka@gmail.com>
2018-11-27 19:47:37 +0300

    Слил все в один поток

--- project/server/src/main.cpp
+++ project/server/src/main.cpp
@@ -5,32 +5,33 @@
 
 #include "../include/MapRender.h"
 
-static sf::Mutex globalMutex;
+//static sf::Mutex globalMutex;
 
-const unsigned short PORT = 5002;
+const unsigned short PORT = 5000;
 
-std::string msgSend;
 bool quit = false;
 
 std::vector<std::unique_ptr<sf::TcpSocket>> socket;
 
 void getInfo() {
-    while(!quit) {
-        for (int i = 0; i < socket.size(); ++i) {
-            if (!msgSend.empty()) {
-                sf::Packet packetSend;
-                globalMutex.lock();
-                std::cout << "Msg = " << msgSend << std::endl;
-                packetSend << msgSend;
-                msgSend = "";
-                globalMutex.unlock();
+    sf::TcpListener listener;
+    if (listener.listen(PORT) != sf::Socket::Done) {
+        puts("Error in listen");
+        exit(1);
+    }
+    listener.setBlocking(false);
+    std::cout << "Server created" << std::endl;
 
-                if (socket[i]->send(packetSend) != sf::Socket::Done) {
-                    std::cout << socket[i]->getRemoteAddress() << ": Disconnect" << std::endl;
-                    socket.erase(socket.end() - 1);
-                    continue;
-                }
-            }
+    while(!quit) {
+        std::unique_ptr<sf::TcpSocket> fooSocket = std::make_unique<sf::TcpSocket>();
+
+        if (listener.accept(*fooSocket) == sf::Socket::Done) {
+            (*fooSocket).setBlocking(false);
+            socket.push_back(std::move(fooSocket));
+            std::cout << socket[socket.size() - 1]->getRemoteAddress() << ": Connected" << std::endl;
+        }
+
+        for (int i = 0; i < socket.size(); ++i) {
 
             sf::Packet packetReceive;
             sf::Socket::Status Status = socket[i]->receive(packetReceive);
@@ -60,59 +61,9 @@ void getInfo() {
     }
 }
 
-void sendInput() {
-    std::string s;
-    getline(std::cin,s);
-    if(s == "exit") {
-        std::cout << "Server Closed" << std::endl;
-        quit = true;
-    }
-    globalMutex.lock();
-    msgSend = s;
-    globalMutex.unlock();
-}
-
-void createServer() {
-    sf::TcpListener listener;
-    if (listener.listen(PORT) != sf::Socket::Done) {
-        puts("Error in listen");
-        exit(1);
-    }
-    listener.setBlocking(false);
-    std::cout << "Server created" << std::endl;
-    while (!quit) {
-        std::unique_ptr<sf::TcpSocket> fooSocket = std::make_unique<sf::TcpSocket>();
-
-        if (listener.accept(*fooSocket) == sf::Socket::Done) {
-            (*fooSocket).setBlocking(false);
-            socket.push_back(std::move(fooSocket));
-            std::cout << socket[socket.size() - 1]->getRemoteAddress() << ": Connected" << std::endl;
-        }
-    }
-}
-
 int main() {
-    sf::Thread* serverCr = nullptr;
-    serverCr = new sf::Thread(&createServer);
-    serverCr->launch();
 
-    sf::Thread * thread = nullptr;
-    thread = new sf::Thread(&getInfo);
-    thread->launch();
-
-    while (!quit) {
-        sendInput();
-    }
-
-    if(serverCr) {
-        serverCr->wait();
-        delete serverCr;
-    }
-
-    if(thread) {
-        thread->wait();
-        delete thread;
-    }
+    getInfo();
 
     return 0;
 
